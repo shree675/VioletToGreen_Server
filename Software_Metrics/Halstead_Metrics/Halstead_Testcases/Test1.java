573926ff997676facdb1dbfa50374723b922afa0
Debeshee <debesheedas@gmail.com>
2022-04-01 15:48:05 +0530

    java parser package added and project structure modified

---
+++ Software_Metrics/Halstead_Metrics/Halstead_Testcases/Test1.java
@@ -0,0 +1,17 @@
+class Test1 {
+
+    public static void main(String[] args) {
+      
+      System.out.println("Enter two numbers");
+      int first = 10;
+      int second = 20;
+      
+      System.out.println(first + " " + second);
+  
+      // add two numbers
+      int sum = first + second;
+      System.out.println("The sum is: " + sum);
+    }
+  }
+
+  // n1 = 2, n2 = 7, N1 = 7, N2 = 13
