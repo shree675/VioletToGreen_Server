0f8a0fcc78ea4c7db7c061c27a328e6201924efc
AASHRITH1903 <cs19b025@iittp.ac.in>
2022-04-02 00:09:07 +0530

    Integrated Cyc

--- Software_Metrics/Cyclomatic_Complexity/README.txt
+++ Software_Metrics/Cyclomatic_Complexity/README.txt
@@ -1,10 +1,19 @@
 Compilation :
-1. CD into the "src" directory.
+1. cd into the /src dir.
 2. Run the command "javac -cp :../lib/antlr-runtime-4.5.2.jar:../lib/json-simple-1.1.1.jar antlr4/*.java grammar/*.java -d ../bin/" 
 3. The compiled output will be saved to the "bin" directory.
 
 Running :
-1. CD into the "bin" directory.
-2. Run the command "java -cp :../lib/antlr-runtime-4.5.2.jar:../lib/json-simple-1.1.1.jar antlr4.Main ../input/Hello.java"
+1. cd into the /bin dir.
+2. Run the command "java -cp :../lib/antlr-runtime-4.5.2.jar:../lib/json-simple-1.1.1.jar antlr4.Main <input_file>.java"
 3. The output is save to output/Output.json
 
+
+
+Alternate Execution:
+1. In the /lib dir, run the command "java -jar out.jar <input_file>.java"
+
+
+To generate the jar file :
+1. In the "bin" dir, run the command "jar cmvf Manifest.txt out.jar antlr4/*.class grammar/*.class".
+2. move the "out.jar" to /lib dir
--- server.js
+++ server.js
@@ -1,2 +1,37 @@
-//This will contain the express server and integrating all server components
+const path = require("path");
+const fs = require("fs");
+const express = require("express");
+const bodyParser = require("body-parser");
+var java = require("java");
 
+const app = express();
+app.use(bodyParser.json());
+
+app.get("/", (req, res) => {
+  res.send("Hello World");
+});
+
+const CYC_PATH = path.join(
+  __dirname,
+  "Software_Metrics",
+  "Cyclomatic_Complexity"
+);
+
+java.classpath.push(path.join(CYC_PATH, "lib", "out.jar"));
+
+app.post("/cyc", (req, res) => {
+  const code = req.body["code"];
+  fs.writeFileSync(path.join(CYC_PATH, "input", "Input.java"), code);
+  var Main = java.import("antlr4.Main");
+  var args = [
+    path.join(CYC_PATH, "input", "Input.java"),
+    path.join(CYC_PATH, "output", "Output.json"),
+  ];
+  Main.mainSync(args);
+  const output = require(path.join(CYC_PATH, "output", "Output.json"));
+  res.json(output);
+});
+
+app.listen(3000, () => {
+  console.log("Server is running on port 3000");
+});
